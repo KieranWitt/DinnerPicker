a0e2aaaff49fa05bc59d8d5e9550f7ddd02a998e
Kieran Witt <Kieran_019@hotmail.com>
2021-05-12 15:59:32 +0100

    Link corrections

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 //Meal objects
-let a = {Food:'Chicken and leek', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/chickenandleek.jpg'};
-let b = {Food:'Spaghetti Bolognese', Link: 'https://www.taste.com.au/recipes/spaghetti-bolognese/335cceba-3913-4172-8a28-44ad7a960ef4', Difficulty: 'Medium', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/spaghettibolognaise.jpg'};
-let c = {Food:'Lasagna', Link: 'https://www.taste.com.au/recipes/our-favourite-lasagne/2ac35c7f-3320-48ef-b2cb-ecef75883241', Difficulty: 'Medium',  Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/lasagna.jpg'};
-let d = {Food:'Roast lamb', Link: 'https://www.taste.com.au/recipes/traditional-roast-lamb/2acb853a-237b-4177-bb85-fdec7977dbae', Difficulty: 'Hard',  Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/roastlamb.jpg'};
-let e = {Food:'Roast chicken', Link: 'https://www.taste.com.au/recipes/basic-roast-chicken/3f2100ad-e41b-44b5-bff5-9f5a27261e97', Difficulty: 'Hard', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/roastchicken.jpg'};
-let f = {Food:'Fajitas', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/fajitas.jpg'};
-let g = {Food:'Tortilla pizza', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/tortillapizza.jpg'};
-let h = {Food:'Spaghetti Carbonara', Link: 'https://www.taste.com.au/recipes/best-spaghetti-carbonara/e121ffdc-2285-4798-b515-3860d1942eb1', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/spaghetticarbonara.jpg'};
-let i = {Food:'Chicken salad', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/chickensalad.jpg'};
-let j = {Food:'Pies and mash', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/pieandmash.jpg'};
-let k = {Food:'BBQ', Link: 'a', Difficulty: 'Medium', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/bbq.jpg'};
-let l = {Food:'Lamb chops and potatoes', Link: 'a', Difficulty: 'Medium', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/lambchops.jpg'};
-let m = {Food:'Takeaway', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/Takeaway.jpg'};
-let n = {Food:'Beans on toast', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/beansontoast.jpg'};
-let o = {Food:'Sausage and mash', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/sausageandmash.jpg'};
-let p = {Food:'Stir fry', Link: 'https://www.taste.com.au/recipes/sticky-chinese-pork-stir-fry/deb19404-4aea-4f5c-ad5e-cf7974109727', Difficulty: 'Medium', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/stirfry.jpg'};
-let q = {Food:'Seasoned Chicken and rice', Link: 'a', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/chickenandrice.jpg'};
-let r = {Food:'Shepards Pie', Link: 'https://www.taste.com.au/recipes/beef-shepherds-pie/70d1e84f-b2c3-4894-a11d-f6036ad5960c', Difficulty: 'Hard', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/shepherdspie.jpg'};
-let s = {Food:'Beef nachos', Link: 'https://www.taste.com.au/recipes/beef-nachos/548cce8f-f9d3-4ca6-9111-029181bc630f', Difficulty: 'Medium', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/beefnachos.jpg'};
-let t = {Food:'Burgers', Link: 'https://www.taste.com.au/recipes/basic-beef-burger/a584ab73-6348-4c78-879e-eb7560965a5f', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/burger.jpg'};
-let u = {Food:'Jacket potato', Link: 'https://www.taste.com.au/recipes/baked-jacket-potatoes/f7f45471-ed48-4cc7-b07b-d1de5a161403', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/jacketpotatoes.jpg'};
-let v = {Food:'Chicken curry', Link: 'https://www.taste.com.au/recipes/chicken-curry-2/bc4b534f-ab9f-4d64-87ec-74250db80a69', Difficulty: 'Medium', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/chickencurry.jpg'};
-let w = {Food:'Chicken enchiladas', Link: 'https://www.taste.com.au/recipes/chicken-enchiladas-tomato-salsa/a577f4e1-bae0-49e4-b888-cce4939f7883', Difficulty: 'Medium', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/chickenenchiladas.jpg'};
-let x = {Food:'Pasta bake', Link: 'https://www.taste.com.au/recipes/chicken-pasta-bake-2/84e050e7-229d-4782-b9e5-ae0186d5293b', Difficulty: 'Medium', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/pastabake.jpg'};
-let y = {Food:'Chicken bake', Link: 'https://www.taste.com.au/recipes/creamy-chicken-bake/dbd3e14e-5973-480c-90ce-52609e89dc7f', Difficulty: 'Hard', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/chickenbake.jpg'};
-let z = {Food:'Chilli Con Carne', Link: 'https://www.taste.com.au/recipes/chilli-con-carne-10/16d484bb-df3d-4069-bbb9-ea5856407344', Difficulty: 'Easy', Image: 'https://github.com/KieranWitt/DinnerPicker/blob/master/images/Chiliconcarni.jpg'};
+let a = {Food:'Chicken and leek', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/chickenandleek.jpg'};
+let b = {Food:'Spaghetti Bolognese', Link: 'https://www.taste.com.au/recipes/spaghetti-bolognese/335cceba-3913-4172-8a28-44ad7a960ef4', Difficulty: 'Medium', Image: 'D:/LearningToCode/DinnerPicker/images/spaghettibolognaise.jpg'};
+let c = {Food:'Lasagna', Link: 'https://www.taste.com.au/recipes/our-favourite-lasagne/2ac35c7f-3320-48ef-b2cb-ecef75883241', Difficulty: 'Medium',  Image: 'D:/LearningToCode/DinnerPicker/images/lasagna.jpg'};
+let d = {Food:'Roast lamb', Link: 'https://www.taste.com.au/recipes/traditional-roast-lamb/2acb853a-237b-4177-bb85-fdec7977dbae', Difficulty: 'Hard',  Image: 'D:/LearningToCode/DinnerPicker/images/roastlamb.jpg'};
+let e = {Food:'Roast chicken', Link: 'https://www.taste.com.au/recipes/basic-roast-chicken/3f2100ad-e41b-44b5-bff5-9f5a27261e97', Difficulty: 'Hard', Image: 'D:/LearningToCode/DinnerPicker/images/roastchicken.jpg'};
+let f = {Food:'Fajitas', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/fajitas.jpg'};
+let g = {Food:'Tortilla pizza', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/tortillapizza.jpg'};
+let h = {Food:'Spaghetti Carbonara', Link: 'https://www.taste.com.au/recipes/best-spaghetti-carbonara/e121ffdc-2285-4798-b515-3860d1942eb1', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/spaghetticarbonara.jpg'};
+let i = {Food:'Chicken salad', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/chickensalad.jpg'};
+let j = {Food:'Pies and mash', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/pieandmash.jpg'};
+let k = {Food:'BBQ', Link: 'a', Difficulty: 'Medium', Image: 'D:/LearningToCode/DinnerPicker/images/bbq.jpg'};
+let l = {Food:'Lamb chops and potatoes', Link: 'a', Difficulty: 'Medium', Image: 'D:/LearningToCode/DinnerPicker/images/lambchops.jpg'};
+let m = {Food:'Takeaway', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/Takeaway.jpg'};
+let n = {Food:'Beans on toast', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/beansontoast.jpg'};
+let o = {Food:'Sausage and mash', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/sausageandmash.jpg'};
+let p = {Food:'Stir fry', Link: 'https://www.taste.com.au/recipes/sticky-chinese-pork-stir-fry/deb19404-4aea-4f5c-ad5e-cf7974109727', Difficulty: 'Medium', Image: 'D:/LearningToCode/DinnerPicker/images/stirfry.jpg'};
+let q = {Food:'Seasoned Chicken and rice', Link: 'a', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/chickenandrice.jpg'};
+let r = {Food:'Shepards Pie', Link: 'https://www.taste.com.au/recipes/beef-shepherds-pie/70d1e84f-b2c3-4894-a11d-f6036ad5960c', Difficulty: 'Hard', Image: 'D:/LearningToCode/DinnerPicker/images/shepherdspie.jpg'};
+let s = {Food:'Beef nachos', Link: 'https://www.taste.com.au/recipes/beef-nachos/548cce8f-f9d3-4ca6-9111-029181bc630f', Difficulty: 'Medium', Image: 'D:/LearningToCode/DinnerPicker/images/beefnachos.jpg'};
+let t = {Food:'Burgers', Link: 'https://www.taste.com.au/recipes/basic-beef-burger/a584ab73-6348-4c78-879e-eb7560965a5f', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/burger.jpg'};
+let u = {Food:'Jacket potato', Link: 'https://www.taste.com.au/recipes/baked-jacket-potatoes/f7f45471-ed48-4cc7-b07b-d1de5a161403', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/jacketpotatoes.jpg'};
+let v = {Food:'Chicken curry', Link: 'https://www.taste.com.au/recipes/chicken-curry-2/bc4b534f-ab9f-4d64-87ec-74250db80a69', Difficulty: 'Medium', Image: 'D:/LearningToCode/DinnerPicker/images/chickencurry.jpg'};
+let w = {Food:'Chicken enchiladas', Link: 'https://www.taste.com.au/recipes/chicken-enchiladas-tomato-salsa/a577f4e1-bae0-49e4-b888-cce4939f7883', Difficulty: 'Medium', Image: 'D:/LearningToCode/DinnerPicker/images/chickenenchiladas.jpg'};
+let x = {Food:'Pasta bake', Link: 'https://www.taste.com.au/recipes/chicken-pasta-bake-2/84e050e7-229d-4782-b9e5-ae0186d5293b', Difficulty: 'Medium', Image: 'D:/LearningToCode/DinnerPicker/images/pastabake.jpg'};
+let y = {Food:'Chicken bake', Link: 'https://www.taste.com.au/recipes/creamy-chicken-bake/dbd3e14e-5973-480c-90ce-52609e89dc7f', Difficulty: 'Hard', Image: 'D:/LearningToCode/DinnerPicker/images/chickenbake.jpg'};
+let z = {Food:'Chilli Con Carne', Link: 'https://www.taste.com.au/recipes/chilli-con-carne-10/16d484bb-df3d-4069-bbb9-ea5856407344', Difficulty: 'Easy', Image: 'D:/LearningToCode/DinnerPicker/images/Chiliconcarni.jpg'};
 
 //Array for meals 
-const mealList = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+const mealList = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]; 
 
 //Array to store the dinners chosen
 let dinnersPicked = [];
